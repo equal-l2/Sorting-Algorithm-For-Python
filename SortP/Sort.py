d63003ba3944cac6fd7ec1f0acccd0b10bb90615
GGBBB2000 <ggbbb@mineo.jp>
2018-01-10 18:16:45 +0900

    Add selection sort

--- SortP/Sort.py
+++ SortP/Sort.py
@@ -18,10 +18,23 @@ def insertion(num):
                 j -= 1
             num[j] = tmp
     return num
-
+def selection(num):
+    for i in range(len(num)):
+        print(num)
+        min = num[i]
+        tmp = num[i]
+        if i < len(num):
+            for j in range(i,len(num)):
+                if min > num[j]:
+                    min = num[j]
+                    num[j] = tmp
+                    num[i] = min
+    return num
 if __name__ == "__main__":
-    num = [9.2,2,31,1,62,5.1,8,7]
+    num = [-9.2,2,31,1,-62,5.1,8,7]
     print("None:"+str(num))
     print("Bubble:"+str(bubble(num)))
-    num = [9.2,2,31,1,62,5.1,8,7]
+    num = ["a","g","g","s","v","r","t","e","w"]
     print("Insertion:"+str(insertion(num)))
+    num = [-9.2,2,31,1,-62,5.1,8,7]
+    print("Selection:"+str(selection(num)))
